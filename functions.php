f101e00d273ceeb7924233265f6da0ac766b4c5a
amedeopro <amedeopro@me.com>
2019-02-14 08:14:44 +0100

    .

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
   if (!function_exists('importStyle')) {
     function importStyle(){
 
+      wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css');
       wp_enqueue_style('boolean', get_template_directory_uri() . '/style.css');
 
     }
