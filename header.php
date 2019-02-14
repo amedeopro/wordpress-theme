ebdef8ead788059e2031cb80bc4668a355d5646c
amedeopro <amedeopro@me.com>
2019-02-14 08:26:39 +0100

    .

--- header.php
+++ header.php
@@ -7,3 +7,14 @@
     <?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?>> <!-- se volessi aggiungere una mia classe basta mettere tra apici le classi che mi interessano separate da uno spazio es classe1 classe2 ecc -->
+
+      <header>
+        <div class="container">
+          <div class="header-left">
+
+          </div>
+          <div class="header-right">
+
+          </div>
+            </div>
+      </header>
