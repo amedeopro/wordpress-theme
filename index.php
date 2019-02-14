13bb8303b304afacd301da76b18bc380ad440584
amedeopro <amedeopro@me.com>
2019-02-14 09:14:51 +0100

    .

--- index.php
+++ index.php
@@ -5,14 +5,17 @@
   <div class="row">
     <div class="col-8">
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <h2>
-          <a href="<?php the_permalink(); ?>">
-            <?php the_title(); ?>
-          </a>
-          <small>
-            <?php the_time('j F Y'); ?>
-          </small>
-        </h2>
+        <div class="post">
+          <h2>
+            <a href="<?php the_permalink(); ?>">
+              <?php the_title(); ?>
+            </a>
+            <small>
+              <?php the_time('j F Y'); ?>
+            </small>
+          </h2>
+          <p><?php the_excerpt(); ?></p>
+        </div>
       <?php endwhile; else: ?>
         <p><?php _e('Nessun articolo corrisponde ai criteri di ricerca.'); ?></p>
       <?php endif; ?>
