f094b645755d2961a0d5e35b5d2de454a216dd6e
amedeopro <amedeopro@me.com>
2019-02-14 09:10:46 +0100

    loop

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
 
 <?php get_header(); ?>
 
-<h1>Ciao Classe 3</h1>
+<div class="container">
+  <div class="row">
+    <div class="col-8">
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <h2>
+          <a href="<?php the_permalink(); ?>">
+            <?php the_title(); ?>
+          </a>
+          <small>
+            <?php the_time('j F Y'); ?>
+          </small>
+        </h2>
+      <?php endwhile; else: ?>
+        <p><?php _e('Nessun articolo corrisponde ai criteri di ricerca.'); ?></p>
+      <?php endif; ?>
+    </div>
+    <div class="col-4">
+      <?php get_sidebar(); ?>
+    </div>
+  </div>
+</div>
 
 
 <?php get_footer(); ?>
